edf3e38796f9be1aa9e74bcee5bc13c8b7321228
Augustin <augustin.paulus@telecom-paris.fr>
2022-11-19 19:58:58 +0100

    made item data understandable

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 
-
+id = 35282
 url = "https://buff.163.com/goods/35282#tab=selling"
 headers = {"Accept-Language": "fr,en-US;q=0.9,en;q=0.8"}
 
@@ -14,11 +14,13 @@ title = soup.find('title')
 print(title)
 
 
-res = requests.get('https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=35282&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&_=1668881309653').json()
+res = requests.get('https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=35282&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&_=1668881309653',headers=headers).json()
 
-for price in res['data']['items']:
-    print(price['price'])
-    print(price['user_id'])
+print(res['data']['goods_infos'][str(id)]['name'])
+
+for item in res['data']['items']:
+    print(item['price'])
+    print(item['user_id'])
 
 
 
