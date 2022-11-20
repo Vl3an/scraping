650743d5a442a2cbdce4428c303e758883500abe
Augustin <augustin.paulus@telecom-paris.fr>
2022-11-20 12:39:03 +0100

    can't solve api issue, will try to convert this project into an web extension later

--- get4xCrafts.py
+++ get4xCrafts.py
@@ -15,13 +15,33 @@ class Get4xCrafts:
         self.headers = parameters.headers
 
         for i in range(nb):
-            self.itemList.append( requests.get(url=self.url, headers = self.headers).json()['data'] )
+            print(Get4xCrafts.getUrl(id,i))
+            r = requests.get(Get4xCrafts.getUrl(id,i), headers = self.headers).json()
 
+            for itemIndex in range(10):
+                if r['code'] == "OK":
+                    self.itemList.append( r['data']['items'][itemIndex] )
+                else: 
+                    print("request error for item id"+str(id)+" at page "+str(i+1)+" : error code "+r['code'])
 
-    def getUrl(id,nb) -> str:
-        return "https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id="+str(id)+"&page_num="+str(nb)+"&sort_by=default&mode=&allow_tradable_cooldown=1&extra_tag_ids=squad_combos&wearless_sticker=1&_=1668903032399"
+    def getStickerPrice(name : str):
+        ###TODO 
+        """get the sticker corresponding id via database"""
+        pass
 
-    def 
+    def getStickerNames(self) -> list[str]:
+        stickerNames = []
+
+        for item in self.itemList:
+            itemStickers = []
+            for sticker in item['asset_info']['info']['stickers']:
+                itemStickers.append(sticker['name'])
+            stickerNames.append(itemStickers)
+        return stickerNames
+
+    def getUrl(id : int,nb : int) -> str:
+        return "https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id="+str(id)+"&page_num="+str(nb+1)+"&sort_by=default&mode=&allow_tradable_cooldown=1&extra_tag_ids=squad_combos&wearless_sticker=1&_=1668942549595"
+
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from pickle import GET
 import GetPriceFromId
-
+import Get4xCrafts
+import time
 getPriceOfM4 = GetPriceFromId.GetPriceFromId(35282)
-getPriceTest = GetPriceFromId.GetPriceFromId(35383)
+#getPriceTest = GetPriceFromId.GetPriceFromId(35383)
 
-print(getPriceOfM4.getName(), getPriceTest.getName())
+#print(getPriceOfM4.getName(), getPriceTest.getName())
 
-print(getPriceOfM4.getLowestPrice(), getPriceTest.getLowestPrice())
+#print(getPriceOfM4.getLowestPrice(), getPriceTest.getLowestPrice())
 
 print(getPriceOfM4.getStickerNames())
 
+
+
+getM4crafts = Get4xCrafts.Get4xCrafts(35282,1)
+
+print(getM4crafts.getStickerNames())
